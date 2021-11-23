68aed5e44c2da605f3dfc52869fb5269f720a38c
keosgoodd <91337148+keosgoodd@users.noreply.github.com>
2021-11-23 00:49:47 -0500

    Update silentaim.lua

--- silentaim.lua
+++ silentaim.lua
@@ -1,4 +1,3 @@
-
 local Aiming = loadstring(game:HttpGet("https://raw.githubusercontent.com/keosgoodd/wjlkwajksdsa/main/silent2.lua"))()
 Aiming.TeamCheck(false)
 
@@ -12,50 +11,50 @@ local Mouse = LocalPlayer:GetMouse()
 local CurrentCamera = Workspace.CurrentCamera
 
 local DaHoodSettings = {
-    SilentAim = true,
-    AimLock = false,
-    Prediction = 0.157,
-    AimLockKeybind = Enum.KeyCode.E
+	SilentAim = true,
+	AimLock = false,
+	Prediction = 0.157,
+	AimLockKeybind = Enum.KeyCode.E
 }
 getgenv().DaHoodSettings = DaHoodSettings
 
 function Aiming.Check()
-    if not (Aiming.Enabled == true and Aiming.Selected ~= LocalPlayer and Aiming.SelectedPart ~= nil) then
-        return false
-    end
+	if not (Aiming.Enabled == true and Aiming.Selected ~= LocalPlayer and Aiming.SelectedPart ~= nil) then
+		return false
+	end
 
-    local Character = Aiming.Character(Aiming.Selected)
-    local KOd = Character:WaitForChild("BodyEffects")["K.O"].Value
-    local Grabbed = Character:FindFirstChild("GRABBING_CONSTRAINT") ~= nil
+	local Character = Aiming.Character(Aiming.Selected)
+	local KOd = Character:WaitForChild("BodyEffects")["K.O"].Value
+	local Grabbed = Character:FindFirstChild("GRABBING_CONSTRAINT") ~= nil
 
-    if (KOd or Grabbed) then
-        return false
-    end
+	if (KOd or Grabbed) then
+		return false
+	end
 
-    return true
+	return true
 end
 
 local __index
 __index = hookmetamethod(game, "__index", function(t, k)
-    if (t:IsA("Mouse") and (k == "Hit" or k == "Target") and Aiming.Check()) then
-        local SelectedPart = Aiming.SelectedPart
+	if (t:IsA("Mouse") and (k == "Hit" or k == "Target") and Aiming.Check()) then
+		local SelectedPart = Aiming.SelectedPart
 
-        if (DaHoodSettings.SilentAim and (k == "Hit" or k == "Target")) then
-            local Hit = SelectedPart.CFrame + (SelectedPart.Velocity * DaHoodSettings.Prediction)
+		if (DaHoodSettings.SilentAim and (k == "Hit" or k == "Target")) then
+			local Hit = SelectedPart.CFrame + (SelectedPart.Velocity * DaHoodSettings.Prediction)
 
-            return (k == "Hit" and Hit or SelectedPart)
-        end
-    end
+			return (k == "Hit" and Hit or SelectedPart)
+		end
+	end
 
-    return __index(t, k)
+	return __index(t, k)
 end)
 
 RunService:BindToRenderStep("AimLock", 0, function()
-    if (DaHoodSettings.AimLock and Aiming.Check() and UserInputService:IsKeyDown(DaHoodSettings.AimLockKeybind)) then
-        local SelectedPart = Aiming.SelectedPart
+	if (DaHoodSettings.AimLock and Aiming.Check() and UserInputService:IsKeyDown(DaHoodSettings.AimLockKeybind)) then
+		local SelectedPart = Aiming.SelectedPart
 
-        local Hit = SelectedPart.CFrame + (SelectedPart.Velocity * DaHoodSettings.Prediction)
+		local Hit = SelectedPart.CFrame + (SelectedPart.Velocity * DaHoodSettings.Prediction)
 
-        CurrentCamera.CFrame = CFrame.lookAt(CurrentCamera.CFrame.Position, Hit.Position)
-    end
-    end)
+		CurrentCamera.CFrame = CFrame.lookAt(CurrentCamera.CFrame.Position, Hit.Position)
+	end
+end)
